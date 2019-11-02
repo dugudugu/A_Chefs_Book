9958c62819dfe9f4823ae05207985f2d481e42bd
Ubuntu <ubuntu@ip-172-31-94-191.ec2.internal>
2019-11-02 17:26:15 +0000

    Added Edit and Delete Button to sigle recipe Layout. Added Confirmation box for Delete button

---
+++ static/js/confirmation.js
@@ -0,0 +1,6 @@
+document.getElementById('deleteConfirm');
+document.addEventListener('click', function(e){
+    if (! confirm("Do you want to remove this recipe?")) {
+        e.preventDefault();
+    }
+})
